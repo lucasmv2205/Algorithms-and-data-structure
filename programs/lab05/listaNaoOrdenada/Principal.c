0af7efe8b4f0ddf23fc5ecbe269ffee77833b4fb
lucasmv2205 <lmartins2205@gmail.com>
2022-06-10 23:51:30 -0300

    lab05 with orderedList and UnOrderedList TADS

---
+++ programs/lab05/listaNaoOrdenada/Principal.c
@@ -0,0 +1,123 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "listaNaoOrdenada.h"
+
+int main()
+{
+  int op;
+  Lista *lst;
+
+  do
+  {
+    do
+    {
+      printf("\n --- LISTAS ESTATICAS SEQUENCIAIS --- \n\n");
+      printf(" Escolha uma opcao\n");
+      printf(" 1. Criar lista\n");
+      printf(" 2. Verificar lista vazia\n");
+      printf(" 3. Verificar lista cheia\n");
+      printf(" 4. Inserir elemento\n");
+      printf(" 5. Remover elemento\n");
+      printf(" 6. Liberar lista\n");
+      printf(" 7. Obter valor de um elemento\n");
+      printf(" 8. Imprimir lista\n");
+      printf(" 9. SAIR\n");
+      printf(" Opcao: ");
+      scanf("%d", &op);
+      if ((op < 1) || (op > 9))
+      {
+
+        printf("\n\n Opcao Invalida! Tente novamente...");
+        system("CLS || clear");
+      }
+    } while ((op < 1) || (op > 9));
+
+    switch (op)
+    {
+    case 1:
+      printf("\n\n Criar lista");
+      lst = inicializa_lista();
+      break;
+
+    case 2:
+      printf("\n\n Ver se lista esta vazia");
+      int flag = lista_vazia(lst);
+      if (flag == 1)
+      {
+        printf("\nLista vazia");
+      }
+      if (flag == 0)
+      {
+        printf("\nLista nao vazia");
+      }
+      break;
+
+    case 3:
+      printf("\n\n Ver se lista esta cheia");
+      int flag2 = lista_cheia(lst);
+      if (flag2 == 1)
+      {
+        printf("\nLista cheia");
+      }
+      if (flag2 == 0)
+      {
+        printf("\nLista nao cheia");
+      }
+      break;
+
+    case 4:
+      printf("\n\n Inserir elemento");
+      int elem;
+      printf("\n Qual elemento deseja inserir: ");
+      scanf("%d", &elem);
+      int flag3 = insere_elemento(lst, elem);
+      if (flag3 == 1)
+      {
+        printf("\n Elemento inserido com sucesso");
+      }
+      if (flag3 == 0)
+      {
+        printf("\nFalha na insercao");
+      }
+      break;
+
+    case 5:
+      printf("\n\n Qual elemento deseja remover: ");
+      int elem2;
+      scanf("%d", &elem2);
+      int flag4 = remove_elemento(lst, elem2);
+      if (flag4 == 1)
+      {
+        printf("\n Elemento removido com sucesso");
+      }
+      if (flag4 == 0)
+      {
+        printf("\nFalha na remocao");
+      }
+      break;
+
+    case 6:
+      printf("\n\n Liberar lista");
+      libera_lista(&lst);
+      break;
+
+    case 7:
+      printf("\n\n Qual posicao do elemento que deseja: ");
+      int pos;
+      scanf("%d", &pos);
+      int elemento_desejado = obtem_valor_elemento(lst, pos);
+      printf("elemento desejado: %d", elemento_desejado);
+      break;
+
+    case 8:
+      printf("\n\n Imprimir lista");
+      imprime_lista(lst);
+      break;
+
+    default:
+      printf("\n\n Pressione qualquer tecla para FINALIZAR...");
+    }
+  } while (op != 9);
+
+  return 0;
+}
---
+++ programs/lab05/listaNaoOrdenada/listaNaoOrdenada.h
@@ -0,0 +1,45 @@
+// Nome: Ponto
+// Dados: numeros reais
+// Lista das operações: gera_pto, set_pto, get_pto, libera_pto, distancia_pto
+typedef struct lista Lista;
+
+// Entradas: nenhuma
+// Pre-condicao: nenhuma
+// Processo: criar um Ponto sem valores de coordenadas definidos e aloca espaco na memoria
+// Saida: endereço de um Ponto criado
+// Pos-condicao: nenhuma
+Lista *inicializa_lista();
+
+// Entradas: endereco de um Ponto e valores do tipo float das coordenadas x e y
+// Pre-condicao: Ponto ter um endereco diferente de nulo
+// Processo: atribuis as coordenadas x e y do Ponto as coordenadas passadas para funcao
+// Saida: 1 caso sucesso no processo, 0 se falha
+// Pos-condicao: Ponto com valores de coordenadas
+int lista_vazia(Lista *lst);
+
+// Entradas: endereco de um Ponto e valores do tipo float das coordenadas x e y
+// Pre-condicao: Ponto ter um endereco diferente de nulo
+// Processo: atribuir aos valores x e y enviados a funcao o valor das coordenadas x e y  do ponto que tambem foi enviado a funcao
+// Saida: 1 caso sucesso no processo, 0 se falha
+// Pos-condicao: os valores do tipo float das coordenadas x e y do ponto
+int lista_cheia(Lista *lst);
+
+// Entradas: endereco de memoria do endereco de memoria de um Ponto
+// Pre-condicao: nenhuma
+// Processo: limpar a memoria utilizada pelo Ponto e limpar seu endereco de memoria tambem
+// Saida: nenhuma
+// Pos-condicao: endereco do ponto liberado
+int insere_elemento(Lista *lst, int elemento);
+
+// Entradas: endereco de dois Pontos
+// Pre-condicao: os Pontos terem enderecos diferentes de nulo
+// Processo: calcular a distancia entre dois Pontos
+// Saida: o valor da distancia entre os Pontos calculada
+// Pos-condicao: nenhuma
+int remove_elemento(Lista *lst, int elemento);
+
+int obtem_valor_elemento(Lista *lst, int pos_elemento);
+
+void libera_lista(Lista **lst);
+
+void imprime_lista(Lista *lst);
