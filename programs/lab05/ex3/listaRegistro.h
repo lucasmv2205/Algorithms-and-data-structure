fa77dc2380bedb590c6d7603acf2e8b11c9aabb7
lucasmv2205 <lmartins2205@gmail.com>
2022-06-12 15:45:10 -0300

    lab05 with documentation of each tad

--- programs/lab05/ex3/listaRegistro.h
+++ programs/lab05/ex3/listaRegistro.h
@@ -1,43 +1,53 @@
-// Nome: Ponto
-// Dados: numeros reais
-// Lista das operações: gera_pto, set_pto, get_pto, libera_pto, distancia_pto
+// Nome: ListaNaoOrdenada
+// Dados: nome(char), volume(inteiro)[ml], preco(float)
+// Lista das operações: inicializa_lista, lista_vazia, lista_cheia, insere_elemento, remove_ultimo, libera_lista, imprime_tabela
 typedef struct lista Lista;
 
 // Entradas: nenhuma
 // Pre-condicao: nenhuma
-// Processo: criar um Ponto sem valores de coordenadas definidos e aloca espaco na memoria
-// Saida: endereço de um Ponto criado
+// Processo: criar uma Lista sem valores definidos e aloca espaco na memoria
+// Saida: endereço de uma Lista criada
 // Pos-condicao: nenhuma
 Lista *inicializa_lista();
 
-// Entradas: endereco de um Ponto e valores do tipo float das coordenadas x e y
-// Pre-condicao: Ponto ter um endereco diferente de nulo
-// Processo: atribuis as coordenadas x e y do Ponto as coordenadas passadas para funcao
-// Saida: 1 caso sucesso no processo, 0 se falha
-// Pos-condicao: Ponto com valores de coordenadas
+// Entradas: endereco de uma Lista
+// Pre-condicao: nenhuma
+// Processo: Verifica se existe algum elemento na lista ou se ela nao possui elementos
+// Saida: 1 caso lista esteja vazia, 0 se nao houver elementos ou lista for nula
+// Pos-condicao: nenhuma
 int lista_vazia(Lista *lst);
 
-// Entradas: endereco de um Ponto e valores do tipo float das coordenadas x e y
-// Pre-condicao: Ponto ter um endereco diferente de nulo
-// Processo: atribuir aos valores x e y enviados a funcao o valor das coordenadas x e y  do ponto que tambem foi enviado a funcao
-// Saida: 1 caso sucesso no processo, 0 se falha
-// Pos-condicao: os valores do tipo float das coordenadas x e y do ponto
+// Entradas: endereco de uma Lista
+// Pre-condicao: lista ser valida
+// Processo: Verifica se a quantidade maxima, 20 elementos, foi atingida
+// Saida: 1 caso lista esteja cheia, 0 se quantidade maxima nao foi atingida, ou seja, lista nao cheia
+// Pos-condicao: nenhuma
 int lista_cheia(Lista *lst);
 
-// Entradas: endereco de memoria do endereco de memoria de um Ponto
-// Pre-condicao: nenhuma
-// Processo: limpar a memoria utilizada pelo Ponto e limpar seu endereco de memoria tambem
-// Saida: nenhuma
-// Pos-condicao: endereco do ponto liberado
+// Entradas: endereco da lista, nome do elemento, seu volume e preco
+// Pre-condicao: lista ser valida e nao estar cheia
+// Processo: inserir registro(nome, volume e preco) no final da lista
+// Saida: 1 se sucesso ou 0 se falha
+// Pos-condicao: lista de entrada com registro a mais
 int insere_elemento(Lista *lst, char *nome, int volume, float preco);
 
-// Entradas: endereco de dois Pontos
-// Pre-condicao: os Pontos terem enderecos diferentes de nulo
-// Processo: calcular a distancia entre dois Pontos
-// Saida: o valor da distancia entre os Pontos calculada
-// Pos-condicao: nenhuma
+// Entradas: endereco de uma lista
+// Pre-condicao: lista ser valida e nao estar vazia
+// Processo: remover o ultimo elemento da lista e decrementar quantidade de elementos da lista
+// Saida: 1 se sucesso ou 0 se falha
+// Pos-condicao: lista de entrada com um elemento a menos(sem o ultimo elemento)
 int remove_ultimo(Lista *lst);
 
+// Entradas: endereco de memoria do endereco de memoria de uma Lista
+// Pre-condicao: nenhuma
+// Processo: limpar a memoria utilizada pela Lista e limpar seu endereco de memoria tambem
+// Saida: nenhuma
+// Pos-condicao: endereco da lista liberado
 void libera_lista(Lista **lst);
 
+// Entradas: endereco da lista
+// Pre-condicao: lista ser valida e nao estar vazia
+// Processo: percorrer a lista e printar cada elemento.
+// Saida: nenhuma
+// Pos-condicao: nenhuma
 void imprime_tabela(Lista *lst);
--- programs/lab05/listaOrdenada/listaOrdenada.h
+++ programs/lab05/listaOrdenada/listaOrdenada.h
@@ -1,45 +1,60 @@
-// Nome: Ponto
-// Dados: numeros reais
-// Lista das operações: gera_pto, set_pto, get_pto, libera_pto, distancia_pto
+// Nome: ListaOrdenada
+// Dados: numeros inteiros
+// Lista das operações: inicializa_lista, lista_vazia, lista_cheia, insere_ord, remove_ord, obtem_valor_elemento, libera_lista, imprime_lista
 typedef struct lista Lista;
 
 // Entradas: nenhuma
 // Pre-condicao: nenhuma
-// Processo: criar um Ponto sem valores de coordenadas definidos e aloca espaco na memoria
-// Saida: endereço de um Ponto criado
+// Processo: criar uma Lista sem valores definidos e aloca espaco na memoria
+// Saida: endereço de uma Lista criada
 // Pos-condicao: nenhuma
 Lista *inicializa_lista();
 
-// Entradas: endereco de um Ponto e valores do tipo float das coordenadas x e y
-// Pre-condicao: Ponto ter um endereco diferente de nulo
-// Processo: atribuis as coordenadas x e y do Ponto as coordenadas passadas para funcao
-// Saida: 1 caso sucesso no processo, 0 se falha
-// Pos-condicao: Ponto com valores de coordenadas
+// Entradas: endereco de uma Lista
+// Pre-condicao: nenhuma
+// Processo: Verifica se existe algum elemento na lista ou se ela nao possui elementos
+// Saida: 1 caso lista esteja vazia, 0 se nao houver elementos ou lista for nula
+// Pos-condicao: nenhuma
 int lista_vazia(Lista *lst);
 
-// Entradas: endereco de um Ponto e valores do tipo float das coordenadas x e y
-// Pre-condicao: Ponto ter um endereco diferente de nulo
-// Processo: atribuir aos valores x e y enviados a funcao o valor das coordenadas x e y  do ponto que tambem foi enviado a funcao
-// Saida: 1 caso sucesso no processo, 0 se falha
-// Pos-condicao: os valores do tipo float das coordenadas x e y do ponto
+// Entradas: endereco de uma Lista
+// Pre-condicao: lista ser valida
+// Processo: Verifica se a quantidade maxima, 20 elementos, foi atingida
+// Saida: 1 caso lista esteja cheia, 0 se quantidade maxima nao foi atingida, ou seja, lista nao cheia
+// Pos-condicao: nenhuma
 int lista_cheia(Lista *lst);
 
-// Entradas: endereco de memoria do endereco de memoria de um Ponto
-// Pre-condicao: nenhuma
-// Processo: limpar a memoria utilizada pelo Ponto e limpar seu endereco de memoria tambem
-// Saida: nenhuma
-// Pos-condicao: endereco do ponto liberado
+// Entradas: endereco de uma lista e numero inteiro a ser inserido
+// Pre-condicao: lista valida e nao estar cheia
+// Processo: percorrer a lista ate encontrar posicao correta de insercao para garantir a ordenacao(proximo for o maior elemento). Inserir o elemento na posicao escolhida.
+// Saida: 1 se sucesso ou 0 se falha
+// Pos-condicao: lista de entrada com um elemento a mais
 int insere_ord(Lista *lst, int elemento);
 
-// Entradas: endereco de dois Pontos
-// Pre-condicao: os Pontos terem enderecos diferentes de nulo
-// Processo: calcular a distancia entre dois Pontos
-// Saida: o valor da distancia entre os Pontos calculada
-// Pos-condicao: nenhuma
+// Entradas: endereco de uma lista e o numero inteiro a ser removido
+// Pre-condicao: lista ser valida e nao estar vazia
+// Processo: percorrer a lista ate econtrar o elemento desejado ou um elemento maior. Se o elemento existe, remover ele da lista.
+// Saida: 1 se sucesso ou 0 se falha
+// Pos-condicao: lista de entrada com um elemento a menos
 int remove_ord(Lista *lst, int elemento);
 
+// Entradas: endereco da lista e elemento desejado
+// Pre-condicao: lista ser valida e nao estar vazia
+// Processo: percorrer a lista ate encontrar o elemento desejado. Se elemento existir, printar elemento.
+// Saida: nenhuma
+// Pos-condicao: nenhuma
 int obtem_valor_elemento(Lista *lst, int pos_elemento);
 
+// Entradas: endereco de memoria do endereco de memoria de uma Lista
+// Pre-condicao: nenhuma
+// Processo: limpar a memoria utilizada pela Lista e limpar seu endereco de memoria tambem
+// Saida: nenhuma
+// Pos-condicao: endereco da lista liberado
 void libera_lista(Lista **lst);
 
+// Entradas: endereco da lista
+// Pre-condicao: lista ser valida e nao estar vazia
+// Processo: percorrer a lista e printar cada elemento.
+// Saida: nenhuma
+// Pos-condicao: nenhuma
 void imprime_lista(Lista *lst);
