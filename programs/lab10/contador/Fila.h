2b00d0c2ecfa391a0d4c2515a134f6e024bdced7
lucasmv2205 <lmartins2205@gmail.com>
2022-07-31 15:00:22 -0300

    lab10 - queues

---
+++ programs/lab10/contador/Fila.h
@@ -0,0 +1,8 @@
+typedef struct fila *Fila;
+
+Fila cria_fila();
+int fila_vazia(Fila fil);
+int fila_cheia(Fila fil);
+int insere_fim(Fila fil, int elem);
+int remove_ini(Fila fil, int *elem);
+int le_final(Fila fil, int *elem);
